97d7ce094daeacd5de0bae48c977f02c0b74f274
Shuhei <shuhei.git@outlook.com>
2020-12-26 20:25:51 +0900

    画像読み込みまで

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var image: UIImage!
+    
+    var nowIndex:Int = 0
+    
+    var imageArray:[UIImage] = [
+        UIImage(named: "img01.jpg")!,
+        UIImage(named: "img02.jpg")!,
+        UIImage(named: "img03.jpg")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = imageArray[0]
     }
 
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    @objc func changeImage(){
+        imageView.image = imageArray[nowIndex]
+    }
+    
+    
+    
 }
 
